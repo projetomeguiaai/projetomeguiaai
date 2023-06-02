9d4a715d0245cf23a32b531f636fc5010867f8a5
Airton <projetomeguia.ai@gmail.com>
2023-06-02 11:57:54 -0300

    arquivos adicionados

---
+++ aplicativo/aplicativo.js
@@ -0,0 +1,11 @@
+function abrirmenu(){
+    let barramenuaberto = document.getElementById ('barra-menu')
+    if (barramenuaberto.style.width == "0px"){
+    barramenuaberto.style.width = "300px"
+    
+    }else{
+    
+        barramenuaberto.style.width = "0px"
+    }
+    
+    }
